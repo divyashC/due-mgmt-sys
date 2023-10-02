d34341f2d861755e40d6ecb19991953b7a3cf6c2
Kiran-sawa <kiransawa8@gmail.com>
2023-10-02 22:24:42 +0600

    Temp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Login from "./pages/Login.js";
 import AddDues from "./pages/AddDues.js";
+import Profile from "./pages/Profile.js"
 const App = () => {
 	return (
 		<>
-			<AddDues />
+			<Profile />
 		</>
 	);
 };
