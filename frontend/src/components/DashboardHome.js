5287b9bbcd75b51ced12f75aa11803385bf849df
Divyash Chhetri <divyashchhetri@gmail.com>
2023-10-03 23:41:00 +0600

    DoughnutChart.js

--- frontend/src/components/DashboardHome.js
+++ frontend/src/components/DashboardHome.js
@@ -1,55 +1,53 @@
-import React, { useState, useEffect } from "react";
-import { Chart } from "primereact/chart";
-
-const DoughnutChartDemo = () => {
-	const [chartData, setChartData] = useState({});
-	const [chartOptions, setChartOptions] = useState({});
-
-	useEffect(() => {
-		const documentStyle = getComputedStyle(document.documentElement);
-		const data = {
-			labels: ["A", "B", "C"],
-			datasets: [
-				{
-					data: [300, 50, 100],
-					backgroundColor: [
-						documentStyle.getPropertyValue("--blue-500"),
-						documentStyle.getPropertyValue("--yellow-500"),
-						documentStyle.getPropertyValue("--green-500"),
-					],
-					hoverBackgroundColor: [
-						documentStyle.getPropertyValue("--blue-400"),
-						documentStyle.getPropertyValue("--yellow-400"),
-						documentStyle.getPropertyValue("--green-400"),
-					],
-				},
-			],
-		};
-		const options = {
-			cutout: "60%",
-		};
-
-		setChartData(data);
-		setChartOptions(options);
-	}, []);
-
-	return (
-		<div className="flex card justify-content-center">
-			<Chart
-				type="doughnut"
-				data={chartData}
-				options={chartOptions}
-				className="w-full md:w-30rem"
-			/>
-		</div>
-	);
-};
+import React from "react";
+import DoughnutChart from "./DoughnutChart";
 
 const DashboardHome = () => {
+	// Dummy data for the first chart
+	const chartData1 = {
+		labels: ["A", "B", "C"],
+		datasets: [
+			{
+				data: [300, 50, 100],
+				backgroundColor: ["#007BFF", "#FFC107", "#28A745"],
+				hoverBackgroundColor: ["#0056b3", "#d39e00", "#1e7e34"],
+			},
+		],
+	};
+
+	const chartOptions1 = {
+		cutout: "60%",
+	};
+
+	// Dummy data for the second chart
+	const chartData2 = {
+		labels: ["X", "Y", "Z"],
+		datasets: [
+			{
+				data: [200, 80, 150],
+				backgroundColor: ["#FF5722", "#FFC107", "#4CAF50"],
+				hoverBackgroundColor: ["#D84315", "#D39E00", "#388E3C"],
+			},
+		],
+	};
+
+	const chartOptions2 = {
+		cutout: "60%",
+	};
+
 	return (
-		<>
-			<DoughnutChartDemo />
-		</>
+		<div className="flex space-x-20">
+			{/* Render two DoughnutCharts */}
+			<DoughnutChart
+				data={chartData1}
+				options={chartOptions1}
+				title="Chart 1 Title"
+			/>
+			<DoughnutChart
+				data={chartData2}
+				options={chartOptions2}
+				title="Chart 2 Title"
+			/>
+		</div>
 	);
 };
 
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -18,64 +18,43 @@ const Dashboard = () => {
 
 	return (
 		<>
-			<button
-				data-drawer-target="default-sidebar"
-				data-drawer-toggle="default-sidebar"
-				aria-controls="default-sidebar"
-				type="button"
-				class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-			>
-				<span class="sr-only">Open sidebar</span>
-				<svg
-					class="w-6 h-6"
-					aria-hidden="true"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						clip-rule="evenodd"
-						fill-rule="evenodd"
-						d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
-					></path>
-				</svg>
-			</button>
-
 			<aside
 				id="default-sidebar"
-				class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+				className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
 				aria-label="Sidebar"
 			>
-				<div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-					<ul class="space-y-2 font-medium">
+				<div className="h-full px-3 py-4 overflow-y-auto bg-gray-50">
+					<ul className="space-y-2 font-medium">
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Dashboard")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-10 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-10 group"
 							>
-								<i class="bi bi-pie-chart-fill"></i>
-								<span class="ml-3">Dashboard</span>
+								<i className="bi bi-pie-chart-fill"></i>
+								<span className="ml-3">Dashboard</span>
 							</a>
 						</li>
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Pending Dues")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-clock-fill"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">Pending Dues</span>
+								<i className="bi bi-clock-fill"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">
+									Pending Dues
+								</span>
 							</a>
 						</li>
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Equipment Monitoring")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-tools"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">
+								<i className="bi bi-tools"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">
 									Equipment Monitoring
 								</span>
 							</a>
@@ -84,47 +63,51 @@ const Dashboard = () => {
 							<a
 								onClick={() => handleMenuItemClick("Accounts")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-cash-coin"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">Accounts</span>
+								<i className="bi bi-cash-coin"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">Accounts</span>
 							</a>
 						</li>
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Procurement")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-clipboard2-plus-fill"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">Procurement</span>
+								<i className="bi bi-clipboard2-plus-fill"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">
+									Procurement
+								</span>
 							</a>
 						</li>
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Add Users")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-person-fill-add"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">Add Users</span>
+								<i className="bi bi-person-fill-add"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">Add Users</span>
 							</a>
 						</li>
 						<li>
 							<a
 								onClick={() => handleMenuItemClick("Laboratories")}
 								href="#"
-								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+								className="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
 							>
-								<i class="bi bi-house-gear-fill"></i>
-								<span class="flex-1 ml-3 whitespace-nowrap">Laboratories</span>
+								<i className="bi bi-house-gear-fill"></i>
+								<span className="flex-1 ml-3 whitespace-nowrap">
+									Laboratories
+								</span>
 							</a>
 						</li>
 					</ul>
 				</div>
 			</aside>
-			<div class="p-4 sm:ml-64 ">
-				<div class="p-2 border-2 border-gray-200 border-dashed rounded-lg flex items-center justify-center">
+			<div className="p-4 sm:ml-64 ">
+				<div className="flex items-center justify-center p-2 border-2 border-gray-200 border-dashed rounded-lg">
 					{activeMenuItem === "Dashboard" && <DashboardHome />}
 					{activeMenuItem === "Pending Dues" && <div>Pending Dues Content</div>}
 					{activeMenuItem === "Equipment Monitoring" && (
