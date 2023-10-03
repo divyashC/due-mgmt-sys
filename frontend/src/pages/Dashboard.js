859486368e3342ae1a9aa060b9987245a719a9cf
Divyash Chhetri <divyashchhetri@gmail.com>
2023-10-03 23:22:54 +0600

    Update Dashboard.js

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,112 +1,142 @@
-import React,{ useState }from 'react'
-import Addusers from "../components/AddUsers.js"
-import Accounts  from '../components/Accounts.js';
-
+import React, { useState } from "react";
+import Addusers from "../components/AddUsers.js";
+import Accounts from "../components/Accounts.js";
 
 const Dashboard = () => {
-
-	const [activeMenuItem, setActiveMenuItem] = useState('Dashboard');
+	const [activeMenuItem, setActiveMenuItem] = useState("Dashboard");
 	const [showAddUsers, setShowAddUsers] = useState(false);
-  
+
 	const handleMenuItemClick = (menuItem) => {
-	  setActiveMenuItem(menuItem);
-	  setShowAddUsers(false);
-	}
-  
+		setActiveMenuItem(menuItem);
+		setShowAddUsers(false);
+	};
+
 	const handleAddUsersClick = () => {
-	  setShowAddUsers(true);
-	}
-  
-  return (
-	<>
-     
-     
-<button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
-   <span class="sr-only">Open sidebar</span>
-   <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-      <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
-   </svg>
-</button>
+		setShowAddUsers(true);
+	};
 
-<aside id="default-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-   <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
-      <ul class="space-y-2 font-medium">
-         <li>
-            <a onClick={() => handleMenuItemClick('Dashboard')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-10 group">
-               <svg class="w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-               </svg>
-               <span class="ml-3">Dashboard</span>
-            </a>
-         </li>
-		 <li>
-            <a onClick={() => handleMenuItemClick('Pending Dues')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Pending Dues</span>
-            </a>
-         </li>
-		 <li>
-            <a onClick={() => handleMenuItemClick('Equipment Monitoring')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Equipment Monitoring</span>
-            </a>
-         </li>
-		 <li>
-            <a onClick={() => handleMenuItemClick('Accounts')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Accounts</span>
-            </a>
-         </li>
-		 <li>
-            <a onClick={() => handleMenuItemClick('Procurement')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Procurement</span>
-            </a>
-         </li>
-          <li>
-            <a onClick={() => handleMenuItemClick('Add Users')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Add Users</span>
-            </a>
-         </li>
-		 <li>
-            <a onClick={() => handleMenuItemClick('Laboratories')} href="#" class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group">
-               <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                  <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-               </svg>
-               <span class="flex-1 ml-3 whitespace-nowrap">Laboratories</span>
-            </a>
-         </li>
-	
-      </ul>
-   </div>
-</aside>
-<div class="p-4 sm:ml-64 " >
-   <div class="p-2 border-2 border-gray-200 border-dashed rounded-lg flex items-center justify-center">
-   {activeMenuItem === 'Dashboard' && <div>Dashboard Content</div>}
-          {activeMenuItem === 'Pending Dues' && <div>Pending Dues Content</div>}
-          {activeMenuItem === 'Equipment Monitoring' && <div>Equipment Monitoring Content</div>}
-          {activeMenuItem === 'Accounts' && <div>Accounts Content</div>}
-          {activeMenuItem === 'Procurement' && <div>Procurement Content</div>}
-          {activeMenuItem === 'Add Users' && <Addusers/> }
-          {activeMenuItem === 'Laboratories' && <div>Laboratories Content</div>}
-   </div>
-</div>
+	return (
+		<>
+			<button
+				data-drawer-target="default-sidebar"
+				data-drawer-toggle="default-sidebar"
+				aria-controls="default-sidebar"
+				type="button"
+				class="inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+			>
+				<span class="sr-only">Open sidebar</span>
+				<svg
+					class="w-6 h-6"
+					aria-hidden="true"
+					fill="currentColor"
+					viewBox="0 0 20 20"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						clip-rule="evenodd"
+						fill-rule="evenodd"
+						d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+					></path>
+				</svg>
+			</button>
 
+			<aside
+				id="default-sidebar"
+				class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+				aria-label="Sidebar"
+			>
+				<div class="h-full px-3 py-4 overflow-y-auto bg-gray-50">
+					<ul class="space-y-2 font-medium">
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Dashboard")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-10 group"
+							>
+								<i class="bi bi-pie-chart-fill"></i>
+								<span class="ml-3">Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Pending Dues")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-clock-fill"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">Pending Dues</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Equipment Monitoring")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-tools"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">
+									Equipment Monitoring
+								</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Accounts")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-cash-coin"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">Accounts</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Procurement")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-clipboard2-plus-fill"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">Procurement</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Add Users")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-person-fill-add"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">Add Users</span>
+							</a>
+						</li>
+						<li>
+							<a
+								onClick={() => handleMenuItemClick("Laboratories")}
+								href="#"
+								class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
+							>
+								<i class="bi bi-house-gear-fill"></i>
+								<span class="flex-1 ml-3 whitespace-nowrap">Laboratories</span>
+							</a>
+						</li>
+					</ul>
+				</div>
+			</aside>
+			<div class="p-4 sm:ml-64 ">
+				<div class="p-2 border-2 border-gray-200 border-dashed rounded-lg flex items-center justify-center">
+					{activeMenuItem === "Dashboard" && <div>Dashboard Content</div>}
+					{activeMenuItem === "Pending Dues" && <div>Pending Dues Content</div>}
+					{activeMenuItem === "Equipment Monitoring" && (
+						<div>Equipment Monitoring Content</div>
+					)}
+					{activeMenuItem === "Accounts" && <div>Accounts Content</div>}
+					{activeMenuItem === "Procurement" && <div>Procurement Content</div>}
+					{activeMenuItem === "Add Users" && <Addusers />}
+					{activeMenuItem === "Laboratories" && <div>Laboratories Content</div>}
+				</div>
+			</div>
+		</>
+	);
+};
 
-    </>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
