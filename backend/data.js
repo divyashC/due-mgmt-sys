11964b2fe769c357e9782c6fd6434bd89ecc0539
Divyash Chhetri <divyashchhetri@gmail.com>
2023-11-06 21:07:20 +0600

    Create data.js

---
+++ backend/data.js
@@ -0,0 +1,41 @@
+// users
+const usersData = [
+	{
+		fullName: "Divyash Chhetri",
+		email: "02200174.cst@rub.edu.bt",
+		phoneNo: "17358766",
+		department: "Information Technology Department",
+		userType: "Student",
+		role: "Year 4 - Sem 7",
+		password: "Cst@12345", // You should hash this password
+	},
+	{
+		fullName: "Karma Chophel",
+		email: "karmachophel.cst@rub.edu.bt",
+		phoneNo: "17171717",
+		department: "Science & Humanities Department",
+		userType: "Staff",
+		role: "Physics Lab Incharge",
+		password: "Cst@12345", // You should hash this password
+	},
+	{
+		fullName: "Passang Dema",
+		email: "passangdema.cst@rub.edu.bt",
+		phoneNo: "17171717",
+		department: "Administration Department",
+		userType: "Admin",
+		role: "Adm Assistant",
+		password: "Cst@12345", // You should hash this password
+	},
+	{
+		fullName: "Jiwan Gurung",
+		email: "jeewangurung.cst@rub.edu.bt",
+		phoneNo: "17171717",
+		department: "ICT Department",
+		userType: "ICT",
+		role: "ICT Officer",
+		password: "Cst@12345", // You should hash this password
+	},
+];
+
+// labs
