64e5d7bc4a641fb021eb7bfd40143e3945f6ef87
Divyash Chhetri <divyashchhetri@gmail.com>
2023-11-08 00:54:52 +0600

    Karsel Search Bar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ value, onChange }) => {
 	return (
 		<form className="flex items-center p-2 bg-gray-100 rounded-lg shadow-lg dark:bg-white">
 			<div className="relative">
@@ -12,15 +12,17 @@ const SearchBar = () => {
 					id="default-search"
 					className="block w-64 p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white dark:border-gray-300 dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-blue-500 dark:focus:border-blue-500"
 					placeholder="Enter Student ID"
+					value={value}
+					onChange={onChange}
 					required
 				/>
 			</div>
-			<button
+			{/* <button
 				type="submit"
 				className="px-3 py-2 ml-2 text-sm font-semibold leading-6 text-white rounded-md shadow-sm bg-sky-950 hover:bg-white hover:text-sky-950 hover:ring-1 hover:ring-inset hover:ring-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 			>
 				Search
-			</button>
+			</button> */}
 		</form>
 	);
 };
