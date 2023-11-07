2ae2d22e462f9003f49df46a932e117b24f97723
Divyash Chhetri <divyashchhetri@gmail.com>
2023-11-07 21:23:07 +0600

    Table Update

---
+++ frontend/src/components/AdministrationTable.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const AdministrationTable = () => {
+	return (
+		<>
+			<div className="relative mx-12 mt-10 overflow-x-auto shadow-md sm:rounded-lg">
+				<table className="w-full text-sm text-left text-gray-500">
+					<thead className="text-xs text-gray-700 uppercase bg-gray-50">
+						<tr>
+							<th scope="col" className="px-6 py-3">
+								SL No.
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<div className="flex items-center">Laboratory</div>
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<div className="flex items-center">Incharge</div>
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<div className="flex items-center">Amount</div>
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<div className="flex items-center">Item Damaged</div>
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<div className="flex items-center">Remarks</div>
+							</th>
+							<th scope="col" className="px-6 py-3">
+								<span className="sr-only">Edit</span>
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr className="bg-white border-b">
+							<th
+								scope="row"
+								className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+							>
+								1
+							</th>
+							<td className="px-6 py-4">Physics Lab</td>
+							<td className="px-6 py-4">Sonam Thinley</td>
+							<td className="px-6 py-4">Nu.340</td>
+							<td className="px-6 py-4">2 Magnifying Class</td>
+
+							<td className="px-6 py-4 text-start">Not Paid</td>
+						</tr>
+
+						<tr className="bg-white border-b">
+							<th
+								scope="row"
+								className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+							>
+								2
+							</th>
+							<td className="px-6 py-4">Physics Lab</td>
+							<td className="px-6 py-4">Sonam Thinley</td>
+							<td className="px-6 py-4">Nu.340</td>
+							<td className="px-6 py-4">Sonometer</td>
+
+							<td className="px-6 py-4 text-start">Not Paid</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
+};
+
+export default AdministrationTable;
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,6 +3,8 @@ import ProfileCard from "../components/ProfileCard.js";
 import Table from "../components/Table.js";
 import SearchBar from "../components/SearchBar.js";
 import AddBtn from "../components/AddBtn.js";
+import StudentTable from "../components/StudentTable.js";
+import AdministrationTable from "../components/AdministrationTable.js";
 
 const Profile = () => {
 	// Get the user type from local storage
@@ -26,7 +28,9 @@ const Profile = () => {
 							<AddBtn />
 						</div>
 					)}
-					<Table />
+					{userType === "Student" && <StudentTable />}
+					{userType === "Staff" && <Table />}
+					{userType === "Admin" && <AdministrationTable />}
 				</div>
 			</div>
 		</>
