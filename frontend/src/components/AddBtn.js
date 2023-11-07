507df507e3d4780bff063f4c1cdec7d81366dfc9
Kiran-sawa <kiransawa8@gmail.com>
2023-11-07 22:00:23 +0600

    temp

--- frontend/src/components/AddBtn.js
+++ frontend/src/components/AddBtn.js
@@ -13,7 +13,7 @@ const AddBtn = () => {
 			className="flex justify-center w-auto px-3 py-2 text-lg font-semibold leading-6 text-white rounded-md shadow-sm bg-sky-950 hover:bg-white hover:text-sky-950 hover:ring-1 hover:ring-inset hover:ring-gray-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 		>
 			<i className="pr-3 bi bi-plus-square hover:text-sky-950"></i>
-			Add
+			Add Dues
 		</Link>
 	);
 };
