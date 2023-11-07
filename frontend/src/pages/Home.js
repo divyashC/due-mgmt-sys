0c1b85e5728f425746068c3423f9796cc61e7022
thinley-choden <120625075+thinley-choden@users.noreply.github.com>
2023-11-08 00:59:16 +0600

    Home Page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+        <div className="about-us-container">
+            <div className="column with-background">
+               
+            </div>
+            <div className="column2">
+			<h5 className="custom-heading">ABOUT US</h5>
+			<h1 className="custom">A "No Due" management system is like a digital assistant for our college, simplifying the way we handle and clear dues.
+			 It keeps everyone in the loop by providing real-time updates, allowing students to check their dues, 
+			 staff to add new ones, and administrators to keep an eye on the outstanding dues, all with ease. By 
+			 going paperless and speeding up the dues clearance process, it helps create a more organized and efficient
+			 college environment, ensuring that dues don't become a hassle for anyone.</h1>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
